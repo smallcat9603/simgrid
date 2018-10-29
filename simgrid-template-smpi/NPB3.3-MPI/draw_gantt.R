714175d0a89790ac73ddd21686efdcb862ffa9d9
smallcat9603 <huyao0107@gmail.com>
2018-10-29 14:47:40 +0900

    test

---
+++ simgrid-template-smpi/NPB3.3-MPI/draw_gantt.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+
+# Read the data
+df_state = read.csv("lu.S.4.state.csv", header=F, strip.white=T)
+names(df_state) = c("Type", "Rank", "Container", "Start", "End", "Duration", "Level", "State");
+df_state = df_state[!(names(df_state) %in% c("Type","Container","Level"))]
+df_state$Rank = as.numeric(gsub("rank-","",df_state$Rank))
+
+# Draw the Gantt Chart
+gc = ggplot(data=df_state) + geom_rect(aes(xmin=Start, xmax=End, ymin=Rank, ymax=Rank+1,fill=State))
+
+# Produce the output
+plot(gc)
+dev.off()
